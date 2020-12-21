33924a7d25dca0ace6619d7e367622c6338a2979
Cory Fladhammer <cjfladham@hotmail.com>
2020-12-20 21:37:27 -0600

    first commit

---
+++ try_it_yourself.py
@@ -0,0 +1,41 @@
+# These are the "Try It Yourself" problems from Python Crash Course
+
+# 2-1
+
+
+# 2-3
+name = "eric"
+personal_message = f"Hello {name.title()}, would you like to learn some Python today?"
+print(personal_message)
+
+# 2-4
+name_case = "gary cannalte"
+print(name_case)
+print(name_case.title())
+print(name_case.upper())
+
+# 2-5 / 2-6
+famous_author = "albert einstein"
+famous_quote = "A person who never made a mistake never tried anything new!"
+sentence = f"{famous_author.title()} once said, \"{famous_quote}\""
+print(sentence)
+
+# 2-7
+
+# 2-8
+addition = 4+4
+subtraction = 16-8
+multiply = 4*2
+divide = 16/2
+
+print(addition)
+print(subtraction)
+print(multiply)
+print(divide)
+
+# 2-9
+favorite_num = 3*9
+num_name = "Cory"
+num_sentence = f"{num_name}'s favorite number is {favorite_num}!"
+print(num_sentence)
+
