9eb4ab4eac89a824ee5e31767e6869114e7584ad
Cory Fladhammer <cjfladham@hotmail.com>
2020-12-24 08:37:59 -0600

    whole lot of nothing

---
+++ motorcycles.py
@@ -0,0 +1,10 @@
+# modify an value in a list
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[2] = 'ducati'
+print(motorcycles)
+
+# add values to a list
+motorcycles.append('harley-davidson')
+print(motorcycles)
